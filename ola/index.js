f9f646d23f976239b820f96bf86573c58fe2fd20
Rafael Benevides <rafabene@gmail.com>
2020-01-11 20:01:23 -0300

    Implemented Session store

--- ola/index.js
+++ ola/index.js
@@ -2,6 +2,8 @@ const tracer = require('./tracer')
 const kafka = require('./kafka-producer')
 const KAFKA_TOPIC = 'my-topic'
 const express = require('express')
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session);
 const tracingMiddleware = require('./tracing-middleware')
 const FORMAT_HTTP_HEADERS = require('opentracing').FORMAT_HTTP_HEADERS
 const app = express()
@@ -13,10 +15,36 @@ const version = (process.env.VERSION == undefined ? "V1" : process.env.VERSION )
 let cont = 0
 let misbehave= false
 
+var sessionStore = new MySQLStore({
+    host: (process.env.MYSQL_HOST == undefined ? "localhost" : process.env.MYSQL_HOST),
+    port: 3306,
+    user: 'myuser',
+    password: 'mypassword',
+    database: 'session'
+})
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    store: (process.env.MYSQL_HOST == undefined ? null : sessionStore),
+    saveUninitialized: true,
+}))
+
 app.use(tracingMiddleware);
 
 app.get('/health', function (req, res){
     res.json({status: 'UP'})
+})
+
+app.get('/session/add/:name', function(request, response) {
+    let name = request.params.name
+    request.session[name] = name
+    console.log(`${name} added to Session`)
+    response.send(request.session)
+})
+
+app.get('/session/get', function(request, response) {
+    response.send(request.session)
 })
 
 app.get('/', [logHeaders, root])
